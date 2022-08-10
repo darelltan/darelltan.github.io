ad0de75d089c21e6835e92acd6e2d4a90d122569
darelltan <77649668+darelltan@users.noreply.github.com>
2022-08-10 18:14:16 +0800

    Delete JS directory

--- JS/animation.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".auto-type", {
-    strings: ["'Hello'", "'Hola'", "'Bonjour'", "'Ni Hao'", "'Hallo'", "'Salve'", "'Ciao'", "'Olá'", "'Konnichiwa'", "'Annyeong Haseyo'", "'Zdravstvuyte'"],
-    typeSpeed: 77,
-    backSpeed: 55,
-    loop: true
-})
-
-
